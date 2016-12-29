e9e611480101d08967f05c0aa603a05538514bc7
Kevin Gibbons <bakkot@gmail.com>
2016-12-28 21:03:19 -0800

    Roll test262: 4ff7e43

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // var zipballUrl = 'https://api.github.com/repos/tc39/test262/zipball'; // this would be nice, but while the API claims to support CORS, it doesn't for this particular endpoint
-var zipballUrl = 'tc39-test262-a73221e.zip';
+var zipballUrl = 'tc39-test262-4ff7e43.zip';
 
 
 // queue/fetch primitives
