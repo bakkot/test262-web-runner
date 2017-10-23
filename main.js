4a8df645e08027068d1fa9cfbc27f4c0a77ae436
Kevin Gibbons <kevin@shapesecurity.com>
2017-10-23 13:42:44 -0700

    $262.uncallableAndIsHTMLDDA() -> $262.fakeDocumentAll

--- main.js
+++ main.js
@@ -132,11 +132,7 @@ function installAPI(global) {
       }
     },
     global: global,
-    uncallableAndIsHTMLDDA: function() {
-      // Note: this actually does not conform to the description given in INTERPRETING.md, which says it should *not* be callable.
-      // That confuses me, so I'm ignoring it.
-      return global.document.all;
-    }
+    IsHTMLDDA: global.document.all,
   };
 }
 
