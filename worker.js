fbfff156340d35e06b73403eb25e45e29eb3540c
Kevin Gibbons <kevin@shapesecurity.com>
2018-11-06 13:12:56 -0800

    handle dynamic imports

--- worker.js
+++ worker.js
@@ -1,8 +1,19 @@
 self.addEventListener('fetch', function(event) {
-  // TODO also intercept blank.html to serve the right thing without hitting the network
-  const isInitial = /blank.html$/.test(event.request.referrer);
-  const isSubsequent = /\/test\/.*\.js$/.test(event.request.referrer);
-  if (isInitial || isSubsequent) {
+  // 'blank.html' resolves for all paths.
+  // We use this to run scripts containing a dynamic import on a synthetic page which has the right URL to serve its depedencies.
+  if (/blank\.html$/.test(event.request.url)) {
+    event.respondWith(Promise.resolve(new Response(
+      `<!doctype html><meta charset=utf-8><title>realm</title>`,
+      {
+        headers: {
+          'Content-Type': 'text/html',
+        }
+      }
+    )));
+    return;
+  }
+
+  if (/\/SYNTHETIC\//.test(event.request.url)) {
     event.respondWith(async function() {
       const client = await clients.get(event.clientId);
       const chan = new MessageChannel();
@@ -19,7 +30,7 @@ self.addEventListener('fetch', function(event) {
           return;
         }
         let text = e.data.data;
-        if (isInitial) {
+        if (/blank.html$/.test(event.request.referrer)) {
           text += '\n;$$testFinished();'; // This is such a hack...
         }
         resolve(new Response(text, {
